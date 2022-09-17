03811f47c2999e3465201c7102a887e0469e59e9
pablomilanese <pabmilanese@gmail.com>
2022-09-17 01:20:22 -0500

    Adding files to map a line.

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,59 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+
+// // Coordinates for each point to be used in the line.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790]
+//   ];
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "orange"
+  }).addTo(map);
+
+// Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// add a circle to the map
+// L.circle([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+//  }).addTo(map);
+
+// add a circle to the map using circleMarker()
+//  L.circleMarker([34.0522, -118.2437]).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000,
+//         color: "orange",
+//     }).addTo(map).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//    });
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
